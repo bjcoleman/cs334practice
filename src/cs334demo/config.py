5909967aa542a331385e23f1d761ea1ddde74664
Ben Coleman <colemanb@moravian.edu>
2020-03-05 17:00:53 -0500

    Add code without tests

--- src/cs334demo/config.py
+++ src/cs334demo/config.py
@@ -6,3 +6,10 @@ class Config:
 
     def get_secret(self):
         return 42
+
+    def code_without_test(self):
+        x = 42
+        y = 73
+        z = 93
+        q = x + y + z
+        return q
